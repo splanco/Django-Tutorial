96baca516d6c816fcd0dbd6b430bce6c08ebb697
Spencer Planco <spencerplanco@gmail.com>
2020-02-21 16:35:17 -0500

    Added a simple test to tests.py and fixed was_published_recently() to return False if pub_date is in the future.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        Make sure was_published_recently() returns false if a question's
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
